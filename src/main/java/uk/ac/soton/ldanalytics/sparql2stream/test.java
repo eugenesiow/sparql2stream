23d3490e895a0550f0cf7019cb5781ce82d9d4ae
Eugene Siow <kyo116@gmail.com>
2016-09-05 17:46:04 +0100

    Add query1 CityBench

--- src/main/java/uk/ac/soton/ldanalytics/sparql2stream/test.java
+++ src/main/java/uk/ac/soton/ldanalytics/sparql2stream/test.java
@@ -167,25 +167,49 @@ public class test {
 //				"} GROUP BY ?sensor \n" + 
 //				"HAVING ( AVG(?temperature) < \"32\"^^xsd:float  &&  MIN(?windSpeed) > \"40.0\"^^xsd:float ) ";
 		//PT10M
-		String queryStr = "PREFIX om-owl: <http://knoesis.wright.edu/ssw/ont/sensor-observation.owl#>\n" + 
-				"PREFIX weather: <http://knoesis.wright.edu/ssw/ont/weather.owl#>\n" + 
-				"PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n" + 
-				"PREFIX owl-time: <http://www.w3.org/2006/time#>\n" + 
+//		String queryStr = "PREFIX om-owl: <http://knoesis.wright.edu/ssw/ont/sensor-observation.owl#>\n" + 
+//				"PREFIX weather: <http://knoesis.wright.edu/ssw/ont/weather.owl#>\n" + 
+//				"PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n" + 
+//				"PREFIX owl-time: <http://www.w3.org/2006/time#>\n" + 
+//				"\n" + 
+//				"SELECT ?sensor\n" + 
+//				"FROM NAMED WINDOW :win ON <http://www.cwi.nl/SRBench/observations> [RANGE 1h]\n" + 
+//				"WHERE {\n" + 
+//				"  WINDOW :win { ?sensor om-owl:generatedObservation [a weather:SnowfallObservation ] ;\n" + 
+//				"          om-owl:generatedObservation ?o1 ;\n" + 
+//				"          om-owl:generatedObservation ?o2 .\n" + 
+//				"  ?o1 a weather:TemperatureObservation ;\n" + 
+//				"      om-owl:observedProperty weather:_AirTemperature ;\n" + 
+//				"      om-owl:result [om-owl:floatValue ?temperature] .\n" + 
+//				"  ?o2 a weather:WindSpeedObservation ;\n" + 
+//				"      om-owl:observedProperty weather:_WindSpeed ;\n" + 
+//				"      om-owl:result [om-owl:floatValue ?windSpeed] . }\n" + 
+//				"} GROUP BY ?sensor \n" + 
+//				"HAVING ( AVG(?temperature) < \"32\"^^xsd:float  &&  MIN(?windSpeed) > \"40.0\"^^xsd:float ) ";
+		String queryStr = "PREFIX sao: <http://purl.oclc.org/NET/sao/>\n" + 
+				"PREFIX ssn: <http://purl.oclc.org/NET/ssnx/ssn#>\n" + 
+				"PREFIX ct: 	<http://www.insight-centre.org/citytraffic#>\n" + 
+				"PREFIX ns: 	<http://www.insight-centre.org/dataset/SampleEventService#>\n" + 
 				"\n" + 
-				"SELECT ?sensor\n" + 
-				"FROM NAMED WINDOW :win ON <http://www.cwi.nl/SRBench/observations> [RANGE 1h]\n" + 
+				"SELECT ?obId1 ?obId2 ?v1 ?v2\n" + 
+				"FROM NAMED WINDOW :traffic1 ON <http://www.insight-centre.org/dataset/SampleEventService#AarhusTrafficData182955> [RANGE 3s]\n" + 
+				"FROM NAMED WINDOW :traffic2 ON <http://www.insight-centre.org/dataset/SampleEventService#AarhusTrafficData158505> [RANGE 3s]\n" + 
 				"WHERE {\n" + 
-				"  WINDOW :win { ?sensor om-owl:generatedObservation [a weather:SnowfallObservation ] ;\n" + 
-				"          om-owl:generatedObservation ?o1 ;\n" + 
-				"          om-owl:generatedObservation ?o2 .\n" + 
-				"  ?o1 a weather:TemperatureObservation ;\n" + 
-				"      om-owl:observedProperty weather:_AirTemperature ;\n" + 
-				"      om-owl:result [om-owl:floatValue ?temperature] .\n" + 
-				"  ?o2 a weather:WindSpeedObservation ;\n" + 
-				"      om-owl:observedProperty weather:_WindSpeed ;\n" + 
-				"      om-owl:result [om-owl:floatValue ?windSpeed] . }\n" + 
-				"} GROUP BY ?sensor \n" + 
-				"HAVING ( AVG(?temperature) < \"32\"^^xsd:float  &&  MIN(?windSpeed) > \"40.0\"^^xsd:float ) ";
+				"	WINDOW :traffic1 {\n" + 
+				"		?obId1 a ssn:Observation;\n" + 
+				"			ssn:observedProperty ?p1;\n" + 
+				"			sao:hasValue ?v1;\n" + 
+				"			ssn:observedBy ns:AarhusTrafficData182955.\n" + 
+				"		?p1 a ct:CongestionLevel.\n" + 
+				"	}\n" + 
+				"	WINDOW :traffic2 {\n" + 
+				"		?obId2 a ssn:Observation;\n" + 
+				"			ssn:observedProperty ?p2;\n" + 
+				"			sao:hasValue ?v2;\n" + 
+				"			ssn:observedBy ns:AarhusTrafficData158505.\n" + 
+				"		?p2 a ct:CongestionLevel.\n" + 
+				"	}\n" + 
+				"}";
 //		String queryStr = "PREFIX ssn: <http://purl.oclc.org/NET/ssnx/ssn#>\n" + 
 //				"PREFIX iotsn: <http://iot.soton.ac.uk/smarthome/sensor#>\n" + 
 //				"PREFIX iot: <http://purl.oclc.org/NET/iot#>\n" + 
