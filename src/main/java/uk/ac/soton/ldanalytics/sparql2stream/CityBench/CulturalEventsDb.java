aed89064201439817bcfff45254e5d0fb2143724
Eugene Siow <kyo116@gmail.com>
2016-09-08 15:36:58 +0100

    Fix q4 CityBench querying static sql source and merge with stream query

---
+++ src/main/java/uk/ac/soton/ldanalytics/sparql2stream/CityBench/CulturalEventsDb.java
@@ -0,0 +1,90 @@
+package uk.ac.soton.ldanalytics.sparql2stream.CityBench;
+
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.QuerySolution;
+import org.apache.jena.query.ResultSet;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.util.FileManager;
+
+public class CulturalEventsDb {
+	private static final String tableName = "AarhusCulturalEvents";
+	
+	public static void main(String[] args) {
+		run("dataset/"+tableName+".n3","./dataset/CityBench");
+	}
+	
+	public static void run(String srcPath, String dbPath) {
+		try {
+			Class.forName("org.h2.Driver");
+			Connection conn = DriverManager.getConnection("jdbc:h2:"+dbPath, "sa", "");
+			createTable(conn);
+			ResultSet results = retriveDataFromRdf(srcPath);
+			insertToTable(conn, results);
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void insertToTable(Connection conn, ResultSet results) throws SQLException {
+		PreparedStatement prep = conn.prepareStatement("INSERT INTO "+tableName+" (title,lat,lon) VALUES (?,?,?)");
+		
+		conn.setAutoCommit(false);
+		while(results.hasNext()) {
+			QuerySolution soln = results.nextSolution() ;
+			prep.setString(1, soln.getLiteral("title").getString());
+			prep.setDouble(2, soln.getLiteral("lat").getDouble());
+			prep.setDouble(3, soln.getLiteral("lon").getDouble());
+			prep.addBatch();
+		}	
+		prep.executeBatch();
+		conn.setAutoCommit(true);
+	}
+
+	private static void createTable(Connection conn) throws SQLException {
+		Statement stat = conn.createStatement();
+		stat.execute("CREATE TABLE "+tableName+"\n" + 
+			"(\n" + 
+			"  title VARCHAR,\n" + 
+			"  lat DOUBLE,\n" + 
+			"  lon DOUBLE\n" + 
+			")\n" + 
+			";");
+	}
+
+	private static ResultSet retriveDataFromRdf(String srcPath) {
+		Model model = ModelFactory.createDefaultModel();
+		InputStream in = FileManager.get().open(srcPath);
+		if (in == null) {
+		    throw new IllegalArgumentException(
+		                                 "File: " + srcPath + " not found");
+		}
+		model.read(in, null, "N3");
+		String queryString = "PREFIX sao: <http://purl.oclc.org/NET/sao/>\n" + 
+				"PREFIX ssn: <http://purl.oclc.org/NET/ssnx/ssn#>\n" + 
+				"PREFIX ct: 	<http://www.insight-centre.org/citytraffic#>\n" + 
+				"\n" + 
+				"SELECT ?title ?lat ?lon\n" + 
+				"WHERE {\n" + 
+				"	?obs a ssn:Observation;\n" + 
+				"		sao:value ?title;\n" + 
+				"		ssn:featureOfInterest ?foi.\n" + 
+				"	?foi ct:hasFirstNode ?fn.\n" + 
+				"	?fn ct:hasLatitude ?lat;\n" + 
+				"		ct:hasLongitude ?lon.\n" + 
+				"}" ;
+		QueryExecution qexec = QueryExecutionFactory.create(queryString,model);
+		return qexec.execSelect();
+	}
+}
--- src/main/java/uk/ac/soton/ldanalytics/sparql2stream/cep/CepEngineCityBench.java
+++ src/main/java/uk/ac/soton/ldanalytics/sparql2stream/cep/CepEngineCityBench.java
@@ -2,12 +2,23 @@ package uk.ac.soton.ldanalytics.sparql2stream.cep;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Map;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.jena.query.Query;
+import org.apache.jena.sparql.algebra.Algebra;
+import org.apache.jena.sparql.algebra.Op;
+import org.apache.jena.sparql.algebra.OpWalker;
 
+import uk.ac.soton.ldanalytics.sparql2sql.model.RdfTableMapping;
+import uk.ac.soton.ldanalytics.sparql2sql.model.RdfTableMappingJena;
+import uk.ac.soton.ldanalytics.sparql2sql.model.SparqlOpVisitor;
+import uk.ac.soton.ldanalytics.sparql2sql.util.SQLFormatter;
 import uk.ac.soton.ldanalytics.sparql2stream.CityBench.TrafficStream;
 import uk.ac.soton.ldanalytics.sparql2stream.CityBench.UserLocationStream;
 import uk.ac.soton.ldanalytics.sparql2stream.CityBench.WeatherStream;
+import uk.ac.soton.ldanalytics.sparql2stream.parser.StreamQueryFactory;
+import uk.ac.soton.ldanalytics.sparql2stream.util.StreamFormatUtil;
 
 import com.espertech.esper.client.Configuration;
 import com.espertech.esper.client.ConfigurationDBRef;
@@ -26,12 +37,7 @@ public class CepEngineCityBench {
 		AarhusTrafficData182955.setupSourceFile("streams/AarhusTrafficData182955.stream");
 		TrafficStream AarhusTrafficData158505 = new TrafficStream(epService,"AarhusTrafficData158505","158505","dataset/trafficMetaData.csv");
 		AarhusTrafficData158505.setupSourceFile("streams/AarhusTrafficData158505.stream");
-		String stmt = "";
-		try {
-			stmt = FileUtils.readFileToString(new File("Queries/CityBench/q1.epl"));
-		} catch(IOException e) {
-			e.printStackTrace();
-		}
+		String stmt = getStatement("q1");
         EPStatement statement = epService.getEPAdministrator().createEPL(stmt);
         statement.addListener(new SimpleQueryListener());
         
@@ -52,12 +58,7 @@ public class CepEngineCityBench {
 		AarhusWeatherData0.setupSourceFile("streams/AarhusWeatherData0.stream");
 		TrafficStream AarhusTrafficData158505 = new TrafficStream(epService,"AarhusTrafficData158505","158505","dataset/trafficMetaData.csv");
 		AarhusTrafficData158505.setupSourceFile("streams/AarhusTrafficData158505.stream");
-		String stmt = "";
-		try {
-			stmt = FileUtils.readFileToString(new File("Queries/CityBench/q2.epl"));
-		} catch(IOException e) {
-			e.printStackTrace();
-		}
+		String stmt = getStatement("q2");
         EPStatement statement = epService.getEPAdministrator().createEPL(stmt);
         statement.addListener(new SimpleQueryListener());
         
@@ -72,18 +73,38 @@ public class CepEngineCityBench {
         epService.destroy();
 	}
 	
+	private static String getStatement(String queryName) {
+		String stmt = null;
+		try {
+			String queryStr = FileUtils.readFileToString(new File("Queries/CityBench/"+queryName+".sparql"));
+			RdfTableMapping mapping = new RdfTableMappingJena();
+			mapping.loadMapping("mapping/SensorRepository.nt");
+			Map<String,String> streamCatalog = StreamFormatUtil.loadStreamCatalog("streams/catalogue.txt");
+			Map<String,String> mappingCatalog = StreamFormatUtil.loadMappingCatalog("streams/mapping_catalogue.txt");
+			Query query = StreamQueryFactory.create(queryStr);
+			Op op = Algebra.compile(query);
+			
+			SparqlOpVisitor v = new SparqlOpVisitor();
+			v.setMappingCatalog(mappingCatalog,"jena");
+			v.useMapping(mapping);
+			v.setNamedGraphs(query.getNamedGraphURIs());
+			v.setStreamCatalog(streamCatalog);
+			OpWalker.walk(op,v);
+			SQLFormatter formatter = new SQLFormatter();		
+			stmt = formatter.format(v.getSQL()).trim();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		return stmt;
+	}
+
 	public static void testQ3() {
 		EPServiceProvider epService = EPServiceProviderManager.getProvider("engine_test");
 		TrafficStream AarhusTrafficData182955 = new TrafficStream(epService,"AarhusTrafficData182955","182955","dataset/trafficMetaData.csv");
 		AarhusTrafficData182955.setupSourceFile("streams/AarhusTrafficData182955.stream");
 		TrafficStream AarhusTrafficData158505 = new TrafficStream(epService,"AarhusTrafficData158505","158505","dataset/trafficMetaData.csv");
 		AarhusTrafficData158505.setupSourceFile("streams/AarhusTrafficData158505.stream");
-		String stmt = "";
-		try {
-			stmt = FileUtils.readFileToString(new File("Queries/CityBench/q3.epl"));
-		} catch(IOException e) {
-			e.printStackTrace();
-		}
+		String stmt = getStatement("q3");
         EPStatement statement = epService.getEPAdministrator().createEPL(stmt);
         statement.addListener(new SimpleQueryListener());
         
@@ -100,10 +121,10 @@ public class CepEngineCityBench {
 	
 	public static void testQ4() {
 		ConfigurationDBRef dbConfig = new ConfigurationDBRef();
-		dbConfig.setDriverManagerConnection("org.postgresql.Driver",
-		                                    "jdbc:postgresql://localhost:5432/database_name", 
-		                                    "user", 
-		                                    "password");
+		dbConfig.setDriverManagerConnection("org.h2.Driver",
+		                                    "jdbc:h2:./dataset/CityBench", 
+		                                    "sa", 
+		                                    "");
 
 		Configuration engineConfig = new Configuration();
 		engineConfig.addDatabaseReference("AarhusCulturalEvents", dbConfig);
@@ -111,12 +132,13 @@ public class CepEngineCityBench {
 		EPServiceProvider epService = EPServiceProviderManager.getProvider("engine_test",engineConfig);
 		UserLocationStream UserLocationService = new UserLocationStream(epService,"UserLocationService");
 		UserLocationService.setupSourceFile("streams/UserLocationService.stream");
-		String stmt = "";
-		try {
-			stmt = FileUtils.readFileToString(new File("Queries/CityBench/q4.epl"));
-		} catch(IOException e) {
-			e.printStackTrace();
-		}
+		String stmt = getStatement("q4");
+//		String stmt = null;
+//		try {
+//			stmt = FileUtils.readFileToString(new File("Queries/CityBench/q4.epl"));
+//		}catch(Exception e) {
+//			e.printStackTrace();
+//		}
         EPStatement statement = epService.getEPAdministrator().createEPL(stmt);
         statement.addListener(new SimpleQueryListener());
         
